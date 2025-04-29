5f923d2f2638e648e0f56ac6a2952b98bd5a3a23
njprem <prem842547@gmail.com>
2025-04-29 14:45:01 +0700

    Add Maintenance route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Public/Login/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import Admin from './pages/Admin/Admin';
-import AircraftPage from './pages/Admin/AircraftPage/AircraftPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Public/Login/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Admin from "./pages/Admin/Admin";
+import AircraftPage from "./pages/Admin/AircraftPage/AircraftPage";
 import AircraftDetailPage from "./pages/Admin/AircraftPage/AircraftDetailPage";
-import NotFound from './pages/NotFound/NotFound';
-import ApiStatusChecker from './components/ApiStatusChecker';
+import NotFound from "./pages/NotFound/NotFound";
+import ApiStatusChecker from "./components/ApiStatusChecker";
+import Maintenance from "./pages/Maintenance/Maintenance";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          
+
           <Route
             path="/admin"
             element={
@@ -29,8 +30,19 @@ function App() {
             <Route path="aircrafts/:id" element={<AircraftDetailPage />} />
           </Route>
 
+          <Route
+            path="/Maintenance"
+            element={
+              <ProtectedRoute allowedRole="admin">
+                <Maintenance />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<div>Helloworld Maintenance</div>} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
+        
       </Router>
     </>
   );
---
+++ src/pages/Maintenance/Maintenance.tsx
@@ -0,0 +1,46 @@
+import { Outlet, NavLink } from 'react-router-dom';
+import './Maintenance.css';
+import { FaWrench } from 'react-icons/fa';
+import { MdAirplaneTicket } from "react-icons/md";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import Navbar from '../../components/Sidebar';
+
+
+
+function Maintenance() {
+  return (
+    <div className="Maintenance-container">
+      
+      {/* Sidebar */}
+      <aside className="sidebar">
+        <div className="sidebar-header">
+         <div className='sidebar-logo'> <MdAirplaneTicket /></div>
+          <div className="airline-name">Airline name</div>
+        </div>
+
+        <NavLink to="/Maintenance" end className={({ isActive }) => isActive ? 'sidebar-item active' : 'sidebar-item'}>
+          <TbLayoutDashboardFilled className="sidebar-icon" />
+          <span>Dashboard</span>
+        </NavLink>
+
+        
+
+        <NavLink to="/Maintenance/Maintenance" className={({ isActive }) => isActive ? 'sidebar-item active' : 'sidebar-item'}>
+          <FaWrench className="sidebar-icon" />
+          <span>Maintenance</span>
+        </NavLink>
+      </aside>
+
+      {/* Main content */}
+      <div className="main-content">
+        <Navbar />
+        <div className="content-area">
+          <Outlet />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Maintenance;
